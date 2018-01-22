21951861b477809d8617605489e122e56ff01fc7
Ashley Gibson <ashley@nosegraze.com>
2018-01-22 16:05:16 +0000

    Exit function if RCP settings ID doesn't exist on page. #1633 (#1634)
    * Exit function if RCP settings ID doesn't exist on page. #1633
    
    * Change if statement to check window.adminpage #1633

--- includes/js/admin-scripts.js
+++ includes/js/admin-scripts.js
@@ -94,6 +94,10 @@ jQuery(document).ready(function($) {
 	//when the history state changes, gets the url from the hash and display
 	$(window).bind( 'hashchange', function(e) {
 
+		if ( ! window.adminpage || ( window.adminpage && 'restrict_page_rcp-settings' !== window.adminpage ) ) {
+			return;
+		}
+
 		var url = jQuery.param.fragment();
 
 		//hide all
