2f83de6db8161c2c4f47abcea9bf887b5b654aa5
Travis Northcutt <travis@travisnorthcutt.com>
2018-03-23 13:11:58 -0600

    Only disable WP toolbar for members (subscriber role)
    Also improves wording.
    
    Closes #1729

--- includes/member-functions.php
+++ includes/member-functions.php
@@ -1318,7 +1318,11 @@ function rcp_maybe_disable_toolbar() {
 
 	global $rcp_options;
 
-	if ( isset( $rcp_options['disable_toolbar'] ) && ! current_user_can( 'manage_options' ) ) {
+	$user = wp_get_current_user();
+	$roles = ( array ) $user->roles;
+	$role = $roles[0];
+
+	if ( isset( $rcp_options['disable_toolbar'] ) && $role == 'subscriber' ) {
 		add_filter( 'show_admin_bar', '__return_false' );
 	}
 }
