7de94785b85b378eaaac536af58ec8c00e059a30
Travis Northcutt <travis@travisnorthcutt.com>
2018-03-28 18:26:57 -0600

    Identify RCP pages in the list table screen
    Closes #1617

--- includes/admin/admin-actions.php
+++ includes/admin/admin-actions.php
@@ -28,3 +28,31 @@ function rcp_process_actions() {
 	}
 }
 add_action( 'admin_init', 'rcp_process_actions' );
+
+/**
+ * Denotes the various RCP pages as such in the pages list table.
+ *
+ * @param array $post_states An array of post display states.
+ * @param WP_Post $post The current post object.
+ */
+function rcp_display_post_states( $post_states, $post ) {
+	$rcp_options = get_option( 'rcp_settings' );
+	$pages       = array(
+		'registration_page' => 'Registration Page',
+		'redirect'          => 'Redirect Page',
+		'account_page'      => 'Account Page',
+		'edit_profile'      => 'Edit Profile Page',
+		'update_card'       => 'Update Card Page'
+	);
+	$pages_array = array();
+	foreach ( $pages as $key => $value ) {
+		$pages_array[ $rcp_options[ $key ] ] = isset( $rcp_options[ $key ] ) ? $pages[ $key ] : false;
+	}
+	if ( array_key_exists( $post->ID, $pages_array ) ) {
+		$post_states[ 'rcp_' . array_search( $pages_array[ $post->ID ], $pages ) ] = $pages_array[ $post->ID ];
+	}
+
+	return $post_states;
+}
+
+add_filter( 'display_post_states', 'rcp_display_post_states', 10, 2 );
